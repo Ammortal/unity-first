7899a505932bf16254cef69f5d0855c8874fe8f0
Ammortal <75484290+Ammortal@users.noreply.github.com>
2022-11-25 22:03:23 +0300

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Hello");
+
+
+        Debug.Log("Привет");
+        Debug.LogWarning("АЛЕРТ");
+        Debug.LogError("КАПЗДА");
+        
+        
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
